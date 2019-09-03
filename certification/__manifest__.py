8adcea60e005ceda6d09d498c0a26db420598fdb
ysfklc <14050111035@ybu.edu.tr>
2019-09-03 09:45:22 +0300

    Certification module added

---
+++ certification/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2014-2015 Grupo ESOC <www.grupoesoc.es>
+# Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Certification",
+    "summary": "Defines certifications for different purposes",
+    'version': '12.0.1.0.0',
+    "category": "Certification Management",
+    "website": "https://github.com/turkmenoglubugra",
+    "author": "Grupo ESOC, Tecnativa, Odoo Community Association (OCA)",
+    "contributors": [
+        'Jairo Llopis <j.llopis@grupoesoc.es>',
+        'Richard deMeester <richard@willowit.com.au>',
+    ],
+    'data': ['security/ir.model.access.csv',
+             'views/certification_bodies_view.xml',
+             'views/certification_view.xml',
+             'views/res_partner_view.xml',
+             'views/standard_view.xml'
+             ],
+    'demo': ['data/certification_data.xml'],
+    "license": "AGPL-3",
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+    'depends': ['base'],
+    'development status':"beta",
+    'maintainers':['ceeficent']
+}
---
+++ certification/models/certification_standard.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+from odoo import models,fields
+
+class CertificationStandard(models.Model):
+
+    _name = 'certification.standard'
+    _description = 'Certification Types'
+
+    name = fields.Char()
+    description = fields.Text()
