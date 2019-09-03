bda6e9c4b5c5ac9edca845b6aeb1e9d887bfd84c
ysfklc <14050111035@ybu.edu.tr>
2019-09-03 09:07:55 +0300

    Certification module added

---
+++ partner_contact_driving_licence/models/res_partner.py
@@ -0,0 +1,14 @@
+# Copyright 2014-2015 Grupo ESOC <www.grupoesoc.es>
+# Copyright 2017-Apertoso N.V. (<http://www.apertoso.be>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    driving_licence = fields.Boolean(
+        string="Driving licence",
+        help= "Indicates if the contact has a driving licence"
+    )
