68db89df945c6b5ea1403ae6ddcf3ba6a6d48eeb
ysfklc <14050111035@ybu.edu.tr>
2019-09-07 08:39:36 +0300

    -

--- rpc.py
+++ rpc.py
@@ -4,11 +4,13 @@ server_url = 'http://localhost:8069'
 db_name = 'test'
 username = 'test@test.com'
 password = 'text'
+
+
 common = client.ServerProxy('%s/xmlrpc/2/common' % server_url)
 version = common.version()
 print(version)
 user_id = common.authenticate(db_name, username, password, {})
 print(user_id)
 models = client.ServerProxy('%s/xmlrpc/2/object' % server_url)
-res = models.execute_kw(db_name, user_id, password, 'hr.employee', 'register_attendance', ['1234'])
+res = models.execute_kw(db_name, user_id, password, 'hr.employee', 'register_attendance', ['1337'])
 print(res)
